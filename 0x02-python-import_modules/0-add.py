7ca85d55adf19f1cce99177715736b42ab66757c
MrsRams2 <t.ramuhashi@gmail.com>
2023-10-22 02:56:42 -0700

    minor fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-      i = 1
-    j = 2
-    print("{} + {} = {}".format(i, j, add(i, j)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
