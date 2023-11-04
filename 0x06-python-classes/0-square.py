f5ea83ac08cde732ce36e2ff601142d8d5d881eb
MrsRams2 <t.ramuhashi@gmail.com>
2023-11-04 12:48:16 -0700

    adding files

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Definition of Square class"""
+
+class Square:
+    """Class body of Square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A class Square"""
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """A new Square initialized"""
+
+        Args:
+            size (int): Size of new square
+            """
+            self.__size = size
