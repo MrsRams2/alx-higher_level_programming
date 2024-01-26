c2e2166cb900c221bb1c3c51f4298e3cc1da85db
MrsRams2 <t.ramuhashi@gmail.com>
2024-01-26 11:52:57 -0800

    a script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+// Print lines using console.log()
+console.log(line1);
+console.log(line2);
+console.log(line3);
